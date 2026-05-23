1182f4a5e9936ae95a91b5c429d0eeacfa092e6f
MyTechBlitz3000 <JustinLeopardoFuji365@outlook.com>
2026-05-23 09:25:24 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,28 +4,35 @@ import PackageDescription
 
 let package = Package(
     name: "NovaFusionDB",
-    
+
     platforms: [
         .iOS(.v17),
         .macOS(.v14)
     ],
-    
+
     products: [
         .executable(
             name: "NovaFusionDB",
             targets: ["NovaFusionDB"]
         )
     ],
-    
+
     dependencies: [
-        
+
     ],
-    
+
     targets: [
+
         .executableTarget(
             name: "NovaFusionDB",
             dependencies: [],
             path: "Sources"
+        ),
+
+        .testTarget(
+            name: "NovaFusionDBTests",
+            dependencies: ["NovaFusionDB"],
+            path: "Tests"
         )
     ]
 )
