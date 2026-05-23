ed1b21eafff338d2844990f6266c381e542e12b7
MyTechBlitz3000 <JustinLeopardoFuji365@outlook.com>
2026-05-23 09:24:24 +0800

    Update run.sh

--- web-dasboard/run.sh
+++ web-dasboard/run.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-echo "🚀 Starting NovaFusionDB..."
+echo "🚀 NovaFusionDB Booting..."
 
-echo ""
-echo "📦 Installing Python dependencies..."
+if ! command -v python3 &> /dev/null
+then
+    echo "❌ Python3 not installed."
+    exit
+fi
+
+if ! command -v pip &> /dev/null
+then
+    echo "❌ pip not installed."
+    exit
+fi
+
+echo "📦 Installing dependencies..."
 pip install -r backend/requirements.txt
 
-echo ""
-echo "🗄️ Starting FastAPI backend..."
-cd backend
+echo "🗄️ Launching Backend..."
 
-python -m uvicorn main:app \
+cd backend || exit
+
+python3 -m uvicorn main:app \
 --host 0.0.0.0 \
 --port 8000 \
 --reload
